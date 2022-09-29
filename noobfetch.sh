6b363384865004c43356861ea0ccc07f2331f9fb
İlker Sezer <75069363+ilker610@users.noreply.github.com>
2022-09-29 21:29:10 +0300

    Main tool

---
+++ noobfetch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cyan='\e[0;36m'
+tp='\e[0m'
+
+source /etc/os-release
+
+if command -v lspci &> /dev/null ; then
+  export gpu=$(lspci | grep "VGA\|3D" | cut -d ":" -f3)
+else
+  export gpu="GPU can't found."
+fi
+
+host="$mobovendor $name $version $mobo"
+
+echo -e "
+    ___   	~ Noob Fetch ~
+   |_^_|  ==========================
+   |   |  $cyan OS:$tp ${PRETTY_NAME}
+  _|   |_ $cyan USER:$tp $(whoami)
+  |__|__| $cyan RAM USAGE:$tp $(free -m | awk 'FNR==2{ print $0 }' | awk '{ print $3}') MB
+          =========================="
