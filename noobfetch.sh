8f9314746c697d57276ced313b16d9168f1862bd
İlker Sezer <75069363+ilker610@users.noreply.github.com>
2022-10-02 22:07:16 +0300

    Update noobfetch.sh

--- noobfetch.sh
+++ noobfetch.sh
@@ -9,7 +9,7 @@ source "/etc/os-release"
 if command -v lspci &> /dev/null ; then
   export gpu=$(lspci | grep "VGA\|3D" | cut -d ":" -f3 | awk 'NR==1{print}')
 else
-  export gpu="WARNING YOU'RE USING a PATATO PC!.."
+  export gpu="Patato xD"
 fi
 
 host="$mobovendor $name $version $mobo"
@@ -22,7 +22,7 @@ echo -e "
   ${red}|   | $cyan OS:\t$tp    ${PRETTY_NAME}
   ${red}|   | $cyan USER:\t$tp    ${USER}
   ${red}|   | $cyan RAM USAGE:$tp $(awk '($1 == "MemFree:") {print($2 / 1024)}' "/proc/meminfo") MB
- ${red}|__|__|$cyan GPU:\t$tp   $gpu
+ ${red}|__|__|$cyan GPU:\t$tp    $gpu
 
 \t==========================
 "
