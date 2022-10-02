f175925c3fe7422bf3005963f8572ac9d3b567b2
lastpingu <54551308+lazypwny751@users.noreply.github.com>
2022-10-02 21:39:04 +0300

    more sus than before

--- noobfetch.sh
+++ noobfetch.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-cyan='\e[0;36m'
-tp='\e[0m'
+cyan="\e[0;36m"
+red="\033[1;31m"
+tp="\e[0m"
 
-source /etc/os-release
+source "/etc/os-release"
 
 if command -v lspci &> /dev/null ; then
-  export gpu=$(lspci | grep "VGA\|3D" | cut -d ":" -f3)
+  export gpu=$(lspci | grep "VGA\|3D" | cut -d ":" -f3 | awk 'NR==1{print}')
 else
-  export gpu="GPU can't found."
+  export gpu="WARNING YOU'RE USING a PATATO PC!.."
 fi
 
 host="$mobovendor $name $version $mobo"
 
 echo -e "
-    ___   	~ Noob Fetch ~
-   |_^_|  ==========================
-   |   |  $cyan SHELL:$tp ${SHELL}
-   |   |  $cyan OS:$tp ${PRETTY_NAME}
-  _|   |_ $cyan USER:$tp $(whoami)
-  |__|__| $cyan RAM USAGE:$tp $(free -m | awk 'FNR==2{ print $0 }' | awk '{ print $3}') MB
-          $cyan GPU:$tp $gpu 
-          =========================="
+\t\t~ Noob Fetch ~
+\t==========================
+   ${red}___${tp}
+  ${red}|_^_|	$cyan SHELL:\t $tp   ${SHELL}
+  ${red}|   | $cyan OS:\t$tp    ${PRETTY_NAME}
+  ${red}|   | $cyan USER:\t$tp    ${USER}
+  ${red}|   | $cyan RAM USAGE:$tp $(awk '($1 == "MemFree:") {print($2 / 1024)}' "/proc/meminfo") MB
+ ${red}|__|__|$cyan GPU:\t$tp   $gpu
+
+\t==========================
+"
